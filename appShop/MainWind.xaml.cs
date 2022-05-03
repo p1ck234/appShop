8bbdf9efff8be1462a089a6f3c3675139980799e
Danil Vetrov <danil-vetrov47@mail.ru>
2022-05-03 15:02:30 +0400

    Подключил новый страницы, все оформил и прописал логику

---
+++ appShop/MainWind.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace appShop
+{
+    /// <summary>
+    /// Interaction logic for MainWind.xaml
+    /// </summary>
+    public partial class MainWind : Window
+    {
+        public MainWind()
+        {
+            InitializeComponent();
+            if (Manager.Admin == true)
+            {
+                tbRole.Text = "Вы вошли как менеджер";
+            }
+            else
+            {
+                tbRole.Text = "Вы вошли как клиент";
+            }
+            Manager.MainFrame = MainFrame;
+            Manager.MainFrame.Navigate(new MainPage());
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                btnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btnBack.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+    }
+}
--- appShop/MainWindow.xaml.cs
+++ appShop/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace appShop
 {
@@ -20,24 +23,83 @@ namespace appShop
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static ModelBD.Model1 bd = new ModelBD.Model1();
+
         public MainWindow()
         {
             InitializeComponent();
+            bd.Users.Load();
         }
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int counter = 0;
 
         private void tbLogin_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            Regex input = new Regex(@"[a-zA-Z]");
+            Match match = input.Match(e.Text);
+            if (!match.Success)
+            {
+                e.Handled = true;
+            }
         }
 
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
-
+            bd.Users.Load();
+            try
+            {
+                if (bd.Users.Local.Where(x => x.login == tbLogin.Text & x.password == tbPass.Password).FirstOrDefault() != null)
+                {
+                    if (bd.Users.Local.Where(x => x.login == tbLogin.Text & x.password == tbPass.Password & x.Admin == true).FirstOrDefault() != null)
+                    {
+                        Manager.Admin = true;
+                    }
+                    else
+                    {
+                        Manager.Admin = false;
+                    }
+                    MainWind mw = new MainWind();
+                    mw.Show();
+                    this.Close();
+                }
+                else
+                {
+                    Exp("Пользователя с такими данными нет!");
+                    counter++;
+                }
+                if (counter >= 3)
+                {
+                    Inf("Вы ввели 3 раза неправильно пароль\nВвод пароля будет доступен через 10 секунд");
+                    counter = 0;
+                    timer = new DispatcherTimer();
+                    timer.Tick += new EventHandler(timer_Tick);
+                    timer.Interval = new TimeSpan(0, 0, 10);
+                    btnEnter.Visibility = Visibility.Hidden;
+                    timer.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Exp(ex.ToString());
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            btnEnter.Visibility = Visibility.Visible;
         }
 
         private void btnQuit_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
+        }
+        public static void Exp(string error)
+        {
+            MessageBox.Show(error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public static void Inf(string inf)
+        {
+            MessageBox.Show(inf, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
